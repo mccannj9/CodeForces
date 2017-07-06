331a004adf7aa7d4972ab904e1498f4376fb84f2
mccannj9 <mccannj@protonmail.com>
2017-07-06 22:59:37 +0200

    Trying a submission, I think it is still too slow

--- Level_Generation.py
+++ Level_Generation.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
-# http://codeforces.com/problemset/problem/822/B
-# Problem name ::: B. Crossword Solving
+# http://codeforces.com/problemset/problem/818/F
+# Problem name ::: F. Level Generation
 # submission number
 
 def max_conn_comp(nodes, edges):
@@ -10,43 +10,35 @@ def max_conn_comp(nodes, edges):
             return nodes - i + 1
 
 
-# def binary_search(nodes):
-#     from math import ceil
-#     max_edges = int(nodes*(nodes-1)/2)
-#     min_edges = int(nodes-1)
-#     min_edges = 0
-#     # search_term = (max_edges+min_edges)/2
-#     print('_________________________________')
-#     print('nodes  |  edges  |  comp  |  bridge ')
-
-#     for search_term in range(min_edges, max_edges + 1):
-#         ccs = max_conn_comp(nodes, search_term)
-#         print("  %s    |   %s   |   %s   |   %s     |" % (nodes, search_term, ccs, ccs-1))
-#     return ccs
-
-
 def binary_search(nodes):
     from math import ceil
-    max_edges = int(nodes*(nodes-1)/2)
     min_edges = 0
-    search_term = (max_edges+min_edges)/2
-    max_edges_found = 0
-    max_bridges_found = 0
-    print("searching for nodes = %s" % nodes)
-
-    for search_term in range(min_edges, max_edges + 1):
+    # maximum of "non-bridge" edges cannot excede nodes
+    max_edges = nodes
+    search_term = ceil((max_edges+min_edges)/2)
+    count = 0
+    while True:
+        # print(nodes, search_term)
         ccs = max_conn_comp(nodes, search_term)
         bridges = ccs - 1
-        total_edges = bridges + search_term
-        if bridges/total_edges >= 0.5:
-            if total_edges > max_edges_found:
-                max_edges_found = total_edges
-                max_bridges_found = bridges
-                total_edges = 0
-    print("max_edges_found = %s" % max_edges_found)
-    print("max_bridges_found = %s" % max_bridges_found)
+        if bridges < search_term:
+            # print(bridges, search_term)
+            max_edges = search_term
+            search_term = ceil(search_term/2)
+        elif bridges > search_term:
+            # print(bridges, search_term)
+            min_edges = search_term
+            search_term = ceil((max_edges+search_term)/2)
+        else:
+            max_edges = bridges + search_term
+            # print("max_edges_found = %s" % max_edges)
+            # print("max_bridges_found = %s" % bridges)
+            return max_edges
+        count += 1
+        if count > max_edges:
+            print("not found")
+            break
 
-    return ccs
 
 
 
@@ -59,6 +51,7 @@ def main():
     graph_sizes = [int(x) for x in data[1:]]
 
     for val in graph_sizes:
+        # print(binary_search(val))
         print(binary_search(val))
 
 
