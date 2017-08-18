22946d276bae2aee295f149a8f21e84a2ba65851
mccannj9 <mccannj@protonmail.com>
2017-08-18 11:12:29 +0200

    Added description of how the problem was solved

--- Level_Generation.py
+++ Level_Generation.py
@@ -8,6 +8,14 @@
 def level_generation(n):
     from math import floor, ceil, sqrt
 
+    # n total nodes where one component has x nodes
+    # the rest of the components have only 1 node each
+    # max edges in x component = (x choose 2) = x^2 - x
+    # we need x^2-x <= 2(n - x) per the problem statement
+    # where n - x is the number of bridges
+    # x^2 + x <= 2n
+    # (x + 1/2)^2-1/4 <= 2n
+    # x <= sqrt(2n+1/4)-1/2
     quad_solv = sqrt(2*n+1/4)-1/2
     x = floor(quad_solv)
     y = ceil(quad_solv)
